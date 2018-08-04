d220a3b2461c0e91d156f09f0c808efc62626ed8
Arjun Bhalla <arjunbhallasg@gmail.com>
2018-08-04 11:23:07 -0500

    Started training functions

--- perceptron.py
+++ perceptron.py
@@ -8,18 +8,21 @@ class Perceptron(object):
 
     Parameters:
 
-    starting_weights: A matrix of shape (n, 1), where n is the number of inputs (ideally dtype = float32).
+    starting_weights: A tf.Variable object of shape (n, 1), where n is the number of inputs (ideally dtype = float32).
+
+    session: tensorflow.Session() object to be used in training.
     
     logistic: A boolean which determines if you want to use the logistic regression function as
     the activation function (if false will use sign).
     """
 
-    def __init__(self, starting_weights, logistic = True):
+    def __init__(self, starting_weights, session, logistic = True):
         
-        assert isinstance(starting_weights, np.matrix)
+        assert isinstance(starting_weights, tf.Variable)
         assert isinstance(logistic, bool)
         self.weights = starting_weights
         self.logistic = logistic
+        self.sess = session
     
     def get_weights(self):
         """
@@ -46,7 +49,7 @@ class Perceptron(object):
         """
         self.logistic = not self.logistic
 
-    def sign_activate(self, x):
+    def _sign_activate(self, x):
         """
         Will return the sign of the given parameter (int or float). 
         """
@@ -56,7 +59,7 @@ class Perceptron(object):
         else:
             return 1
     
-    def logistic_activate(self, x):
+    def _logistic_activate(self, x):
         """
         Will run the sigmoid function on the given parameter (int or float).
         """
@@ -69,10 +72,29 @@ class Perceptron(object):
         x must be a vector (nx1 matrix) (tensorflow / numpy).
         """
         assert(isinstance(x, np.matrix))
-        digit = tf.matmul(self.weights, x, transpose_b = True)
+        digit = tf.matmul(self.weights, x, transpose_b = True)[0][0]
 
         if self.logistic:
-            return self.logistic_activate(digit)
+            return self._logistic_activate(digit)
         else:
-            return self.sign_activate(digit)
-        
+            return self._sign_activate(digit)
+    
+    def train(self, x_values, output_vector, learning_rate):
+        """
+        Will train the perceptron according to the relevant (in-built) cost function.
+
+        x_values: (1, m) np.matrix where m is the number of features in the example.
+        output_vector: (1,) np.matrix which denotes the expected output value for this example.
+        learning_rate: The alpha value for this perceptron.
+        """
+
+        if self.get_logistic:
+            self._train_logistic(x_values, output_vector, learning_rate)
+        else:
+            self._train_sign(x_values, output_vector, learning_rate)
+    
+    def _train_logistic(x_values, output_vector, learning_rate):
+        pass
+    
+    def _train_sign(x_values, output_vector, learning_rate):
+        pass
