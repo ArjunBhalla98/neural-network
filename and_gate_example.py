0dafa802485047db9f61db484b9d66e91367a79f
Arjun Bhalla <arjunbhallasg@gmail.com>
2018-08-07 03:38:10 +0800

    Changed train to take in a matrix for inputs and outputs

--- and_gate_example.py
+++ and_gate_example.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 import math 
 import perceptron
 
@@ -13,11 +14,27 @@ train_in = [
 
 train_out = [
     [F],
-    [T],
-    [T],
+    [F],
+    [F],
     [T]
 ]
 
-percep = perceptron.Perceptron(tf.Variable(tf.ones((3,1))), tf.Session(), False)
+percep = perceptron.Perceptron(tf.Variable(tf.ones((3,1)), dtype = "float32"), tf.Session(), logistic = False)
+ALPHA = 0.1
+TRAINING_ITERATIONS = 20
 
-print(percep.predict([train_in[0]]))
+print("BEFORE: \n")
+print("F F: " + str(percep.predict(train_in[0])))
+print("F T: " + str(percep.predict(train_in[1])))
+print("T F: " + str(percep.predict(train_in[2])))
+print("T T: " + str(percep.predict(train_in[3])))
+print()
+
+for i in range(TRAINING_ITERATIONS):
+    percep.train(train_in, train_out, ALPHA)
+
+print("AFTER:\n")
+print("F F: " + str(percep.predict(train_in[0])))
+print("F T: " + str(percep.predict(train_in[1])))
+print("T F: " + str(percep.predict(train_in[2])))
+print("T T: " + str(percep.predict(train_in[3])))
--- perceptron.py
+++ perceptron.py
@@ -16,8 +16,6 @@ class Perceptron(object):
     the activation function (if false will use sign).
     """
 
-    
-
     def __init__(self, starting_weights, session, logistic = True):
         
         assert isinstance(starting_weights, tf.Variable)
@@ -34,9 +32,8 @@ class Perceptron(object):
     
     def set_weights(self, arg):
         """
-        Sets the weights to a custom value. Must be a matrix in the shape of (?, 1).
+        Sets the weights to a custom value. Must be a tf.Variable in the shape of (n, 1).
         """
-        assert (np.shape(arg) == np.shape(np.zeros((None, 1))))
         self.weights = arg
     
     def get_logistic(self):
@@ -77,13 +74,13 @@ class Perceptron(object):
 
         returns: float
         """
-        print(x)
+        
         init = tf.global_variables_initializer()
         self.sess.run(init)
-        digit = self.sess.run(tf.matmul(x, self.sess.run(self.weights)))[0][0]
+        digit = self.sess.run(tf.matmul([x], tf.cast(self.weights, tf.float32)))[0][0]
         
 
-        if self.logistic:
+        if self.get_logistic():
             return self._logistic_activate(digit)
         else:
             return self._sign_activate(digit)
@@ -92,12 +89,12 @@ class Perceptron(object):
         """
         Will train the perceptron according to the relevant (in-built) cost function.
 
-        x_values: (1, n) np.matrix where n is the number of features in the example.
-        output_vector: (1,) np.matrix which denotes the expected output value for this example.
+        x_values: (m, n) np.matrix where n is the number of features in the example, m is the # of examples.
+        output_vector: (m, 1) matrix which denotes the expected output value for this example.
         learning_rate: The alpha value for this perceptron.
         """
 
-        if self.get_logistic:
+        if self.get_logistic():
             self._train_logistic(x_values, output_vector, learning_rate)
         else:
             self._train_sign(x_values, output_vector, learning_rate)
@@ -111,6 +108,19 @@ class Perceptron(object):
         self.sess.run(trainer)
     
     def _train_sign(self, x_values, output_vector, learning_rate):
-        loss = (self.predict(x_values) - output_vector[0])*learning_rate
-        update_values = np.multiply(x_values, loss) # This is what to add to the weights, is the same shape as x_values (1xn)
-        np.add(self.weights, np.transpose(update_values)) # weights + updates_transpose = 1xn + 1xn. This is the update.
+        
+        predicted_output = tf.matmul(x_values, self.get_weights()) # Gets the what the perceptron thinks the output should be (Vector mx1)
+        loss = tf.reduce_sum(tf.multiply(tf.subtract(predicted_output, output_vector), learning_rate))
+        print(self.sess.run(loss))
+        update_term = tf.multiply(x_values, self.sess.run(loss))
+        self.set_weights(tf.add(self.get_weights(), update_term))
+
+        # loss = (self.predict(x_values) - output_vector[0])*learning_rate
+        # print("Loss: " + str(loss))
+        # update_values = np.multiply(x_values, loss) # This is what to add to the weights, is the same shape as x_values (1xn)
+        # print("Update vals: " + str(update_values))
+        # update = tf.Variable(np.add(self.sess.run(self.weights), np.transpose(update_values))) # weights + updates_transpose = 1xn + 1xn. This is the update.
+        # init = tf.global_variables_initializer()
+        # self.sess.run(init)
+        # print("New weights: " + str(self.sess.run(update)))
+        # self.set_weights(update)
